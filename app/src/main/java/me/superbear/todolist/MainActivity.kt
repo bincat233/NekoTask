f6acb377d6bc3385ca8a30bd9b6a975e75d3e254
Zijia <isxiongzj@gmail.com>
2026-07-13 05:22:45 +0800

    fix(locale): migrate MainActivity to AppCompatActivity and declare AppLocalesMetadataHolderService

--- app/src/main/java/me/superbear/todolist/MainActivity.kt
+++ app/src/main/java/me/superbear/todolist/MainActivity.kt
@@ -1,13 +1,13 @@
 package me.superbear.todolist
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.setContent
 import androidx.core.view.WindowCompat
 import me.superbear.todolist.ui.main.MainScreen
 import me.superbear.todolist.ui.theme.TodolistTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
